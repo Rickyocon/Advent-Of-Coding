e767f0f33f1ce2921bb951f406daa2429932b03d
Rickyocon <rickyoconnor2313@gmail.com>
2026-01-21 14:03:25 -0500

    added main function for clarity

--- 2025/Day 2/Challenge 1/Day2Challenge1.py
+++ 2025/Day 2/Challenge 1/Day2Challenge1.py
@@ -1,17 +1,10 @@
-# Global Variables
-invalid_inputs = []
-
 # --------------------------------------------------
-
-puzzle_input = "2025\Day 2\Challenge 1\PuzzelInput.txt"
 
 def file_to_list(path_to_input_file):
     with open(path_to_input_file, 'r') as file:
         line = file.read().strip()
         items = line.split(',')
     return items
-
-list_of_input = file_to_list(puzzle_input)
 
 # --------------------------------------------------
 
@@ -38,16 +31,23 @@ def get_range(string):
 
 # --------------------------------------------------
 
-for range_string in list_of_input:
-    numbers_in_range = get_range(range_string)
-    for number_string in numbers_in_range:
-        if is_pattern_number(number_string):
-            # Append to a list of all invalid inputs
-            invalid_inputs.append(number_string)
+def main():
 
-# Convert to integers for summing
-invalid_input_values = [int(value) for value in invalid_inputs]
+    puzzle_input = "2025\Day 2\Challenge 1\PuzzelInput.txt"
+    invalid_inputs = []
 
-total = sum(invalid_input_values)
+    list_of_input = file_to_list(puzzle_input)
 
-print(total)
+    for range_string in list_of_input:
+        numbers_in_range = get_range(range_string)
+        for number_string in numbers_in_range:
+            if is_pattern_number(number_string):
+                invalid_inputs.append(number_string)
+
+    invalid_input_values = [int(value) for value in invalid_inputs]
+    total = sum(invalid_input_values)
+    print("The answer to this set of inputs is:",total)
+
+if __name__ == "__main__":
+    main()
+
