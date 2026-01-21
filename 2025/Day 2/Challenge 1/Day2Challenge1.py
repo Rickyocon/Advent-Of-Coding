0f95626c1c099a7c171474211120cb1371d61e38
Rickyocon <rickyoconnor2313@gmail.com>
2026-01-21 13:51:22 -0500

    renamed all variables for clarity

--- 2025/Day 2/Challenge 1/Day2Challenge1.py
+++ 2025/Day 2/Challenge 1/Day2Challenge1.py
@@ -1,57 +1,53 @@
-#Global Variables
-ListOfInvaildInputs = []
+# Global Variables
+invalid_inputs = []
 
-#--------------------------------------------------
+# --------------------------------------------------
 
-PuzzelInput = "2025\Day 2\Challenge 1\PuzzelInput.txt"
+puzzle_input = "2025\Day 2\Challenge 1\PuzzelInput.txt"
 
-def FileToList(PathToInputFile):
-    with open(PathToInputFile, 'r') as File:
-        line = File.read().strip()
-        list = line.split(',')
-    return list
+def file_to_list(path_to_input_file):
+    with open(path_to_input_file, 'r') as file:
+        line = file.read().strip()
+        items = line.split(',')
+    return items
 
-ListOfInput = FileToList(PuzzelInput)
+list_of_input = file_to_list(puzzle_input)
 
-#-------------------------------------------------- 
+# --------------------------------------------------
 
-def IsPatternNumber(string):
-    #Split string ing half and store each half to be compared
-    FirstHalf = string[:len(string)//2] # take length of string and then split it in half -> colon in front means get eveything from the begining until the half
-    SecondHalf =  string[len(string)//2:] # take length of string and then split it in half -> colon at end means get eveything from the half until the end
-    if FirstHalf == SecondHalf:
-        return True
-    else:
-        return False
+def is_pattern_number(string):
+    # Split string in half and store each half to be compared
+    first_half = string[:len(string)//2]  # everything from beginning until the half
+    second_half = string[len(string)//2:]  # everything from half until the end
+    return first_half == second_half
     
-#--------------------------------------------------
+# --------------------------------------------------
 
-def GetRange(string):
-    #Strings in the list are ranges of numbers which look something like this 11-35 for example so need to split into the two min and max and then convert to int and then get all numbers in between
+def get_range(string):
+    # Strings in the list are ranges like "11-35"
     bounds = string.split("-")
-    min = bounds[0]
-    max = bounds[1]
+    min_str = bounds[0]
+    max_str = bounds[1]
     
-    minNum = int(min)
-    maxNum = int(max)
+    min_num = int(min_str)
+    max_num = int(max_str)
 
-    ListOfNumbersInRange = list(range(minNum,maxNum+1))
-    ConvertListOfNumbersToStrings = [str(num) for num in ListOfNumbersInRange]
-    return ConvertListOfNumbersToStrings
+    list_of_numbers_in_range = list(range(min_num, max_num+1))
+    list_of_strings = [str(num) for num in list_of_numbers_in_range]
+    return list_of_strings
 
-#--------------------------------------------------
+# --------------------------------------------------
 
-for numbers in ListOfInput:
-    StringOfNumbersInFirstRange = GetRange(numbers)
-    for string in StringOfNumbersInFirstRange:
-        InvaildInput = IsPatternNumber(string)
-        if InvaildInput == True:
-            #Append to a list of all invalid inputs
-            ListOfInvaildInputs.append(string)
+for range_string in list_of_input:
+    numbers_in_range = get_range(range_string)
+    for number_string in numbers_in_range:
+        if is_pattern_number(number_string):
+            # Append to a list of all invalid inputs
+            invalid_inputs.append(number_string)
 
+# Convert to integers for summing
+invalid_input_values = [int(value) for value in invalid_inputs]
 
-NumberValuesOfInvalidInputs = [int(value) for value in ListOfInvaildInputs]
+total = sum(invalid_input_values)
 
-Total = sum(NumberValuesOfInvalidInputs)
-
-print(Total)
+print(total)
